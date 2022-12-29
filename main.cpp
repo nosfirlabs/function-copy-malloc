52dca7b39dd5a0051499a193605c85ded6d1d01b
NOSFIR! labs <80362772+nosfirlabs@users.noreply.github.com>
2022-12-29 20:07:07 +0100

    fix error

--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <errno.h>
 
-// Dies ist die Funktion, die wir zur Laufzeit duplizieren möchten
+// This is the function we want to duplicate at runtime
 int original_function(int x, int y) {
   int result = 0;
   for (int i = 0; i < x; i++) {
@@ -14,37 +15,38 @@ int original_function(int x, int y) {
   return result;
 }
 
-
 int main() {
-  // Öffne einen Dateideskriptor für die ausführbare Datei
+  // Open a file descriptor for the executable
   int fd = open("/proc/self/exe", O_RDONLY);
   if (fd < 0) {
-    perror("Fehler beim Öffnen der ausführbaren Datei");
+    perror("Failed to open executable");
     return 1;
   }
-  // Ermittle die Größe der originalen Funktion
+
+  // Get the size of the original function
   size_t size = (char*)original_function - (char*)main;
-  
-   // Mappe den Maschinencode der originalen Funktion mithilfe von mmap in den Speicher
-  void* original_code = mmap(NULL, size, PROT_READ | PROT_EXEC, MAP_PRIVATE, fd, (char*)original_function - (char*)main);
+
+  // Map the machine code for the original function into memory using mmap
+  void* original_code = mmap(NULL, size, PROT_READ | PROT_EXEC, MAP_PRIVATE, fd, (char*)main - (char*)main);
   if (original_code == MAP_FAILED) {
-    perror("Fehler beim Mappen der originalen Funktion");
+    fprintf(stderr, "Failed to map original function: %s\n", strerror(errno));
     return 1;
   }
-  
-  // Alloziere Speicher für die Duplikatfunktion mithilfe von malloc
+
+  // Allocate memory for the duplicate function using malloc
   void* duplicate = malloc(size);
-  
-   // Kopiere den Maschinencode der originalen Funktion in die Duplikatfunktion
+
+  // Copy the machine code for the original function to the duplicate function
   memcpy(duplicate, original_code, size);
-  
-  // Caste die Duplikatfunktion zu einem Funktionszeiger und rufe sie auf
+
+  // Cast the duplicate function to a function pointer and call it
   int (*duplicate_function)(int, int) = (int (*)(int, int))duplicate;
   printf("Result: %d\n", duplicate_function(5, 7));
-  
-  // Unmappe den originalen Code und gib den allozierten Speicher frei
+
+  // Unmap the original code and free the allocated memory
   munmap(original_code, size);
   free(duplicate);
 
   return 0;
 }
+
