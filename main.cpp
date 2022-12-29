4d3edc3f8ebc992c99dd63ba882f9f6feb880f38
NOSFIR! labs <80362772+nosfirlabs@users.noreply.github.com>
2022-12-29 19:57:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -22,8 +22,14 @@ int main() {
     perror("Fehler beim Öffnen der ausführbaren Datei");
     return 1;
   }
-
+  // Ermittle die Größe der originalen Funktion
+  size_t size = (char*)original_function - (char*)main;
+  
+   // Mappe den Maschinencode der originalen Funktion mithilfe von mmap in den Speicher
+  void* original_code = mmap(NULL, size, PROT_READ | PROT_EXEC, MAP_PRIVATE, fd, (char*)original_function - (char*)main);
+  if (original_code == MAP_FAILED) {
+    perror("Fehler beim Mappen der originalen Funktion");
+    return 1;
+  }
+  
 }
-
-// Ermittle die Größe der originalen Funktion
-size_t size = (char*)original_function - (char*)main;
